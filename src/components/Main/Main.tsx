2f0def07b3bf2b9aa28b9a97c6c92bcdd57ffb57
De Leon <manuel.de.leon@accenture.com>
2022-10-15 09:41:10 -0600

    Started with countdown

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,86 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { sharedCount } from "../../misc/sharedState";
 
 const StyledMain = styled.header`
-  height: 10rem;
+  padding: 30px;
   background: ${(props) => props.theme.global.body_color};
 `;
+
+const StyledHeader = styled.h1`
+  text-align: center;
+  font-size: 6vw;
+  line-height: 1.1;
+  font-style: normal;
+  font-weight: 400;
+  margin-bottom: 24px;
+`;
+
+const StyledRemaining = styled(StyledHeader)`
+  font-size: 4.5vw;
+`;
+
+const StyledInfo = styled.p`
+  text-align: center;
+  font-family: "Orbikular", serif;
+  font-size: 1.3333em;
+  line-height: 1.2;
+  font-style: normal;
+  font-weight: 300;
+  letter-spacing: -0.01em;
+  margin-top: 100px;
+`;
+
+const StyledTimeBlock = styled.span`
+  margin: 1.5vw;
+`;
+
 export const Main = () => {
-  const [count, setCount] = sharedCount();
+  const launchDate = dayjs().add(0, "days").add(3, "hours").add(5, "seconds");
+  const currentDate = dayjs();
+  const [daysRemaining, setDaysRemaining] = useState(
+    launchDate.diff(currentDate, "days")
+  );
+  const [hoursRemaining, setHoursRemaining] = useState(
+    launchDate.diff(currentDate, "hours") % 24
+  );
+  const [minutesRemaining, setMinutesRemaining] = useState(
+    launchDate.diff(currentDate, "minutes") % 60
+  );
+  const [secondsRemaining, setSecondsRemaining] = useState(
+    launchDate.diff(currentDate, "seconds") % 60
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentDate = dayjs();
+      setDaysRemaining(launchDate.diff(currentDate, "days"));
+      setHoursRemaining(launchDate.diff(currentDate, "hours") % 24);
+      setMinutesRemaining(launchDate.diff(currentDate, "minutes") % 60);
+      setSecondsRemaining(launchDate.diff(currentDate, "seconds") % 60);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <StyledMain>
-      <button onClick={() => setCount(count + 1)}>
-        <>In Main {count}</>
-      </button>
+      <StyledHeader>Our product will launch in</StyledHeader>
+      <StyledRemaining>
+        <StyledTimeBlock>
+          {daysRemaining} day{daysRemaining === 1 ? "" : "s"}
+        </StyledTimeBlock>
+        <StyledTimeBlock>
+          {hoursRemaining} hour{hoursRemaining === 1 ? "" : "s"}
+        </StyledTimeBlock>
+        <StyledTimeBlock>
+          {minutesRemaining} minute{minutesRemaining === 1 ? "" : "s"}
+        </StyledTimeBlock>
+        <StyledTimeBlock>
+          {secondsRemaining} second{secondsRemaining === 1 ? "" : "s"}
+        </StyledTimeBlock>
+      </StyledRemaining>
+      <StyledInfo>More info on how to update the countdown.</StyledInfo>
     </StyledMain>
   );
 };
--- src/misc/sharedState.ts
+++ src/misc/sharedState.ts
@@ -2,4 +2,3 @@ import { User } from "../models/User";
 import { shared } from "./shared";
 
 export const [sharedUser, updateSharedUser] = shared<User | null>(null);
-export const [sharedCount, updateSharedCount] = shared(0);
