d37efe25fdfe1c72f8700927c39c6717795018bf
De Leon <manuel.de.leon@accenture.com>
2022-10-16 06:41:00 -0600

    Add Jest

---
+++ __tests__/functions.test.tsx
@@ -0,0 +1,30 @@
+import { isValidEmail } from "../src/misc/functions";
+
+describe("functions", () => {
+  describe("isValidEmail", () => {
+    it("return true with valid email", () => {
+      const value = isValidEmail("me@me.com");
+      expect(value).toBe(true);
+    });
+
+    it("return false with invalid email 1", () => {
+      const value = isValidEmail("me@me.");
+      expect(value).toBe(false);
+    });
+
+    it("return false with invalid email 2", () => {
+      const value = isValidEmail("me@");
+      expect(value).toBe(false);
+    });
+
+    it("return false with invalid email 3", () => {
+      const value = isValidEmail("me");
+      expect(value).toBe(false);
+    });
+
+    it("return false with invalid email 4", () => {
+      const value = isValidEmail("");
+      expect(value).toBe(false);
+    });
+  });
+});
