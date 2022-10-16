6e519b1ba14dae6688dc031d5c66e979e402d95e
De Leon <manuel.de.leon@accenture.com>
2022-10-16 16:19:17 -0600

    Clean tests

--- src/components/CreateAccount/CreateAccount.test.tsx
+++ src/components/CreateAccount/CreateAccount.test.tsx
@@ -25,16 +25,87 @@ const routerPush = jest.fn();
 }));
 
 describe("CreateAccount", () => {
-  describe("Initial state", () => {
-    beforeEach(() => {
-      createAccountReturn = new Promise<void>((r) => r());
-      render(
-        <ThemeProvider theme={{ global: {} }}>
-          <CreateAccount />
-        </ThemeProvider>
-      );
+  beforeEach(() => {
+    createAccountReturn = new Promise<void>((r) => r());
+    render(
+      <ThemeProvider theme={{ global: {} }}>
+        <CreateAccount />
+      </ThemeProvider>
+    );
+  });
+
+  test("Doesn't have email error", () => {
+    const emailError = screen.queryByText("Please enter your email");
+    expect(emailError).toBeFalsy();
+  });
+  test("Doesn't have password error", () => {
+    const emailError = screen.queryByText(
+      "Password must be at least 10 characters long"
+    );
+    expect(emailError).toBeFalsy();
+  });
+
+  describe("Click Create Account", () => {
+    beforeEach(async () => {
+      const button = screen.getByText("Create Account");
+      await userEvent.click(button);
     });
 
+    test("Didn't call createAccount", () => {
+      expect(createAccount).not.toHaveBeenCalled();
+    });
+
+    test("Shows email error", () => {
+      const emailError = screen.queryByText("Please enter your email");
+      expect(emailError).toBeTruthy();
+    });
+    test("Shows password error", () => {
+      const emailError = screen.queryByText(
+        "Password must be at least 10 characters long"
+      );
+      expect(emailError).toBeTruthy();
+    });
+  });
+
+  describe("Enter bad email", () => {
+    beforeEach(() => {
+      const input = screen
+        .getByPlaceholderText("Email Address")
+        .closest("input");
+      input && fireEvent.change(input, { target: { value: "my@" } });
+    });
+    test("Shows email error", () => {
+      const emailError = screen.queryByText("Please enter your email");
+      expect(emailError).toBeTruthy();
+    });
+  });
+
+  describe("Enter bad password", () => {
+    beforeEach(() => {
+      const input = screen.getByPlaceholderText("Password").closest("input");
+      input && fireEvent.change(input, { target: { value: "123456789" } });
+    });
+    test("Shows password error", () => {
+      const passwordError = screen.queryByText(
+        "Password must be at least 10 characters long"
+      );
+      expect(passwordError).toBeTruthy();
+    });
+  });
+
+  describe("Enter valid email and password", () => {
+    beforeEach(() => {
+      const inputEmail = screen
+        .getByPlaceholderText("Email Address")
+        .closest("input");
+      const inputPassword = screen
+        .getByPlaceholderText("Password")
+        .closest("input");
+      inputEmail &&
+        fireEvent.change(inputEmail, { target: { value: "my@domain.com" } });
+      inputPassword &&
+        fireEvent.change(inputPassword, { target: { value: "1234567890" } });
+    });
     test("Doesn't have email error", () => {
       const emailError = screen.queryByText("Please enter your email");
       expect(emailError).toBeFalsy();
@@ -52,111 +123,38 @@ describe("CreateAccount", () => {
         await userEvent.click(button);
       });
 
-      test("Didn't call createAccount", () => {
-        expect(createAccount).not.toHaveBeenCalled();
-      });
-
-      test("Shows email error", () => {
-        const emailError = screen.queryByText("Please enter your email");
-        expect(emailError).toBeTruthy();
-      });
-      test("Shows password error", () => {
-        const emailError = screen.queryByText(
-          "Password must be at least 10 characters long"
-        );
-        expect(emailError).toBeTruthy();
+      test("Redirects to home", () => {
+        expect(routerPush).toHaveBeenCalledWith("/");
       });
     });
 
-    describe("Enter bad email", () => {
-      beforeEach(() => {
-        const input = screen
-          .getByPlaceholderText("Email Address")
-          .closest("input");
-        input && fireEvent.change(input, { target: { value: "my@" } });
-      });
-      test("Shows email error", () => {
-        const emailError = screen.queryByText("Please enter your email");
-        expect(emailError).toBeTruthy();
-      });
-    });
+    const errorMessages = {
+      "auth/email-already-in-use":
+        "We already have an account with that email.",
+      "Something else": "Something else",
+    };
 
-    describe("Enter bad password", () => {
-      beforeEach(() => {
-        const input = screen.getByPlaceholderText("Password").closest("input");
-        input && fireEvent.change(input, { target: { value: "123456789" } });
-      });
-      test("Shows password error", () => {
-        const passwordError = screen.queryByText(
-          "Password must be at least 10 characters long"
-        );
-        expect(passwordError).toBeTruthy();
-      });
-    });
-
-    describe("Enter valid email and password", () => {
-      beforeEach(() => {
-        const inputEmail = screen
-          .getByPlaceholderText("Email Address")
-          .closest("input");
-        const inputPassword = screen
-          .getByPlaceholderText("Password")
-          .closest("input");
-        inputEmail &&
-          fireEvent.change(inputEmail, { target: { value: "my@domain.com" } });
-        inputPassword &&
-          fireEvent.change(inputPassword, { target: { value: "1234567890" } });
-      });
-      test("Doesn't have email error", () => {
-        const emailError = screen.queryByText("Please enter your email");
-        expect(emailError).toBeFalsy();
-      });
-      test("Doesn't have password error", () => {
-        const emailError = screen.queryByText(
-          "Password must be at least 10 characters long"
-        );
-        expect(emailError).toBeFalsy();
-      });
-
-      describe("Click Create Account", () => {
+    for (let [errorCode, displayMessage] of Object.entries(errorMessages)) {
+      describe(`createAccount returns ${errorCode}`, () => {
         beforeEach(async () => {
-          const button = screen.getByText("Create Account");
-          await userEvent.click(button);
+          createAccountReturn = new Promise<void>((resolve, reject) =>
+            reject({ code: errorCode })
+          );
+          createAccountReturn.catch(() => undefined);
         });
 
-        test("Redirects to home", () => {
-          expect(routerPush).toHaveBeenCalledWith("/");
+        describe("Click Create Account", () => {
+          beforeEach(async () => {
+            const button = screen.getByText("Create Account");
+            await userEvent.click(button);
+          });
+
+          test(`Displays error: ${displayMessage}`, () => {
+            const passwordError = screen.queryByText(displayMessage);
+            expect(passwordError).toBeTruthy();
+          });
         });
       });
-
-      const errorMessages = {
-        "auth/email-already-in-use":
-          "We already have an account with that email.",
-        "Something else": "Something else",
-      };
-
-      for (let [errorCode, displayMessage] of Object.entries(errorMessages)) {
-        describe(`createAccount returns ${errorCode}`, () => {
-          beforeEach(async () => {
-            createAccountReturn = new Promise<void>((resolve, reject) =>
-              reject({ code: errorCode })
-            );
-            createAccountReturn.catch(() => undefined);
-          });
-
-          describe("Click Create Account", () => {
-            beforeEach(async () => {
-              const button = screen.getByText("Create Account");
-              await userEvent.click(button);
-            });
-
-            test(`Displays error: ${displayMessage}`, () => {
-              const passwordError = screen.queryByText(displayMessage);
-              expect(passwordError).toBeTruthy();
-            });
-          });
-        });
-      }
-    });
+    }
   });
 });
--- src/components/Login/Login.test.tsx
+++ src/components/Login/Login.test.tsx
@@ -25,28 +25,99 @@ const routerPush = jest.fn();
 }));
 
 describe("Login", () => {
-  describe("Initial state", () => {
-    beforeEach(() => {
-      signInReturn = new Promise<void>((r) => r());
-      render(
-        <ThemeProvider theme={{ global: {} }}>
-          <Login />
-        </ThemeProvider>
+  beforeEach(() => {
+    signInReturn = new Promise<void>((r) => r());
+    render(
+      <ThemeProvider theme={{ global: {} }}>
+        <Login />
+      </ThemeProvider>
+    );
+  });
+
+  describe("Links", () => {
+    test("Has create account link", () => {
+      const link = screen.getByText("Create account").closest("a");
+      expect(link).toHaveAttribute("href", "/createAccount");
+    });
+
+    test("Has forgot password link", () => {
+      const link = screen.getByText("Forgot password?").closest("a");
+      expect(link).toHaveAttribute("href", "/forgotPassword");
+    });
+  });
+
+  test("Doesn't have email error", () => {
+    const emailError = screen.queryByText("Please enter your email");
+    expect(emailError).toBeFalsy();
+  });
+  test("Doesn't have password error", () => {
+    const emailError = screen.queryByText(
+      "Password must be at least 10 characters long"
+    );
+    expect(emailError).toBeFalsy();
+  });
+
+  describe("Click Login", () => {
+    beforeEach(async () => {
+      const loginButton = screen.getByText("Login");
+      await userEvent.click(loginButton);
+    });
+
+    test("Didn't call signIn", () => {
+      expect(signIn).not.toHaveBeenCalled();
+    });
+
+    test("Shows email error", () => {
+      const emailError = screen.queryByText("Please enter your email");
+      expect(emailError).toBeTruthy();
+    });
+    test("Shows password error", () => {
+      const emailError = screen.queryByText(
+        "Password must be at least 10 characters long"
       );
+      expect(emailError).toBeTruthy();
     });
+  });
 
-    describe("Links", () => {
-      test("Has create account link", () => {
-        const link = screen.getByText("Create account").closest("a");
-        expect(link).toHaveAttribute("href", "/createAccount");
-      });
-
-      test("Has forgot password link", () => {
-        const link = screen.getByText("Forgot password?").closest("a");
-        expect(link).toHaveAttribute("href", "/forgotPassword");
-      });
+  describe("Enter bad email", () => {
+    beforeEach(() => {
+      const input = screen
+        .getByPlaceholderText("Email Address")
+        .closest("input");
+      input && fireEvent.change(input, { target: { value: "my@" } });
     });
+    test("Shows email error", () => {
+      const emailError = screen.queryByText("Please enter your email");
+      expect(emailError).toBeTruthy();
+    });
+  });
 
+  describe("Enter bad password", () => {
+    beforeEach(() => {
+      const input = screen.getByPlaceholderText("Password").closest("input");
+      input && fireEvent.change(input, { target: { value: "123456789" } });
+    });
+    test("Shows password error", () => {
+      const passwordError = screen.queryByText(
+        "Password must be at least 10 characters long"
+      );
+      expect(passwordError).toBeTruthy();
+    });
+  });
+
+  describe("Enter valid email and password", () => {
+    beforeEach(() => {
+      const inputEmail = screen
+        .getByPlaceholderText("Email Address")
+        .closest("input");
+      const inputPassword = screen
+        .getByPlaceholderText("Password")
+        .closest("input");
+      inputEmail &&
+        fireEvent.change(inputEmail, { target: { value: "my@domain.com" } });
+      inputPassword &&
+        fireEvent.change(inputPassword, { target: { value: "1234567890" } });
+    });
     test("Doesn't have email error", () => {
       const emailError = screen.queryByText("Please enter your email");
       expect(emailError).toBeFalsy();
@@ -64,111 +135,38 @@ describe("Login", () => {
         await userEvent.click(loginButton);
       });
 
-      test("Didn't call signIn", () => {
-        expect(signIn).not.toHaveBeenCalled();
-      });
-
-      test("Shows email error", () => {
-        const emailError = screen.queryByText("Please enter your email");
-        expect(emailError).toBeTruthy();
-      });
-      test("Shows password error", () => {
-        const emailError = screen.queryByText(
-          "Password must be at least 10 characters long"
-        );
-        expect(emailError).toBeTruthy();
+      test("Redirects to home", () => {
+        expect(routerPush).toHaveBeenCalledWith("/");
       });
     });
 
-    describe("Enter bad email", () => {
-      beforeEach(() => {
-        const input = screen
-          .getByPlaceholderText("Email Address")
-          .closest("input");
-        input && fireEvent.change(input, { target: { value: "my@" } });
-      });
-      test("Shows email error", () => {
-        const emailError = screen.queryByText("Please enter your email");
-        expect(emailError).toBeTruthy();
-      });
-    });
+    const errorMessages = {
+      "auth/wrong-password": "Wrong password.",
+      "auth/user-not-found": "Email not found.",
+      "Something else": "Something else",
+    };
 
-    describe("Enter bad password", () => {
-      beforeEach(() => {
-        const input = screen.getByPlaceholderText("Password").closest("input");
-        input && fireEvent.change(input, { target: { value: "123456789" } });
-      });
-      test("Shows password error", () => {
-        const passwordError = screen.queryByText(
-          "Password must be at least 10 characters long"
-        );
-        expect(passwordError).toBeTruthy();
-      });
-    });
-
-    describe("Enter valid email and password", () => {
-      beforeEach(() => {
-        const inputEmail = screen
-          .getByPlaceholderText("Email Address")
-          .closest("input");
-        const inputPassword = screen
-          .getByPlaceholderText("Password")
-          .closest("input");
-        inputEmail &&
-          fireEvent.change(inputEmail, { target: { value: "my@domain.com" } });
-        inputPassword &&
-          fireEvent.change(inputPassword, { target: { value: "1234567890" } });
-      });
-      test("Doesn't have email error", () => {
-        const emailError = screen.queryByText("Please enter your email");
-        expect(emailError).toBeFalsy();
-      });
-      test("Doesn't have password error", () => {
-        const emailError = screen.queryByText(
-          "Password must be at least 10 characters long"
-        );
-        expect(emailError).toBeFalsy();
-      });
-
-      describe("Click Login", () => {
+    for (let [errorCode, displayMessage] of Object.entries(errorMessages)) {
+      describe(`signIn returns ${errorCode}`, () => {
         beforeEach(async () => {
-          const loginButton = screen.getByText("Login");
-          await userEvent.click(loginButton);
+          signInReturn = new Promise<void>((resolve, reject) =>
+            reject({ code: errorCode })
+          );
+          signInReturn.catch(() => undefined);
         });
 
-        test("Redirects to home", () => {
-          expect(routerPush).toHaveBeenCalledWith("/");
+        describe("Click Login", () => {
+          beforeEach(async () => {
+            const loginButton = screen.getByText("Login");
+            await userEvent.click(loginButton);
+          });
+
+          test(`Displays error: ${displayMessage}`, () => {
+            const passwordError = screen.queryByText(displayMessage);
+            expect(passwordError).toBeTruthy();
+          });
         });
       });
-
-      const errorMessages = {
-        "auth/wrong-password": "Wrong password.",
-        "auth/user-not-found": "Email not found.",
-        "Something else": "Something else",
-      };
-
-      for (let [errorCode, displayMessage] of Object.entries(errorMessages)) {
-        describe(`signIn returns ${errorCode}`, () => {
-          beforeEach(async () => {
-            signInReturn = new Promise<void>((resolve, reject) =>
-              reject({ code: errorCode })
-            );
-            signInReturn.catch(() => undefined);
-          });
-
-          describe("Click Login", () => {
-            beforeEach(async () => {
-              const loginButton = screen.getByText("Login");
-              await userEvent.click(loginButton);
-            });
-
-            test(`Displays error: ${displayMessage}`, () => {
-              const passwordError = screen.queryByText(displayMessage);
-              expect(passwordError).toBeTruthy();
-            });
-          });
-        });
-      }
-    });
+    }
   });
 });
